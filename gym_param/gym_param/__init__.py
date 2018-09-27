e2614c0250ead252633b8c78a4a2841b3b08d210
nakul <nakulgopalan@gmail.com>
2018-09-26 20:27:05 -0400

    removed pacakge at outer level

--- gym_param/gym_param/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Cartpole-param-v0',
-    entry_point='gym_param.gym_param.envs:CartpoleParamEnv',
-)
--- gym_param/gym_param/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-
-"""Cartpole param Gym Enviornments."""
-
-# from gym_param.envs.cartpole_param_env import CartpoleParamEnv
-from gym_param.gym_param.envs.cartpole_param_env import CartpoleParamEnv
