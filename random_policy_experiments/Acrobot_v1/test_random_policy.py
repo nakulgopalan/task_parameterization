3bf338fc34bb3d95c30c76aadcd082abcb2de88d
nakulgopalan <nakulgopalan@gmail.com>
2018-10-13 18:01:49 -0400

    changed to acrobot

--- random_policy_experiments/Acrobot_v1/test_random_policy.py
+++ random_policy_experiments/Acrobot_v1/test_random_policy.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # PARAMETERS
 t0 = time.time()
 
-policies_to_test = 10
+policies_to_test = 10000
 episodes_for_evaluation = 100
 
 env = gym.make('Acrobot-v1')
@@ -45,21 +45,33 @@ t1 = time.time()
 
 total_n = t1-t0
 
-print("time taken: " + total_n)
+print("time taken: " + str(total_n))
 
-# plot
-fig, ax = plt.subplots()
+
 
 meanr = np.zeros(policies_to_test)
+minlen = np.zeros(policies_to_test)
+tosave = np.zeros((policies_to_test, 8))
 for i in range(policies_to_test):
     meanr[i] = results[i][0]
+    minlen[i] = results[i][1]
+    tosave[i, :] = np.concatenate((np.array([results[i][0], results[i][1]], dtype=np.float), results[i][2]))
 
+
+# plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 12))
 # the histogram of the data
-ax.hist(meanr, 200)
-ax.set_yscale('log')
-ax.set_xlabel('Mean Steps')
-ax.set_ylabel('# Random Policies')
-ax.set_title('Mean Steps vs. Policies ({0})'.format(policies_to_test))
+ax1.hist(minlen, episodes_for_evaluation)
+ax1.set_yscale('log')
+ax1.set_xlabel('Episodes')
+ax1.set_ylabel('# Random Policies')
+# ax1.set_title('Histogram of Minimum Length from Random Policies on the Acrobot Gym Environment vs. Policies ({0})'.format(policies_to_test))
+
+ax2.hist(meanr, 200)
+ax2.set_yscale('log')
+ax2.set_xlabel('Mean Reward')
+ax2.set_ylabel('# Random Policies')
+# ax2.set_title('Histogram of Mean Reward from Random policies on the Acrobot Gym Environment vs. Policies ({0})'.format(policies_to_test))
 
 plt.savefig('{0}.png'.format(policies_to_test))
 plt.show()
